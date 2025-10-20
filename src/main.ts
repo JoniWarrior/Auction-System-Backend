418673e663d72b45979309d7029d87d231223f76
JoniWarrior <joniashimi1@gmail.com>
2025-10-20 14:23:19 +0200

    Change Place of Loggin Interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ErrorResponseNormalizerFilter } from './filter/error-response-normalizer.filter';
-import { AuditInterceptor } from 'interceptors/main.interceptor';
+import { AuditInterceptor } from 'src/interceptors/main.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
