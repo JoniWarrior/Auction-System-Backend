367c5a17892fbd8640327ddb2ad3b703c34a8838
Edmond Alimehmeti <edi.alimehmeti@gmail.com>
2025-10-22 16:58:22 +0200

    filter fix

--- src/filter/error-response-normalizer.filter.ts
+++ src/filter/error-response-normalizer.filter.ts
@@ -5,9 +5,8 @@ import {
   HttpStatus,
   Logger,
 } from '@nestjs/common';
-import { ValidationError } from 'joi';
 import { Response } from 'express';
-import moment from 'moment';
+import { ValidationError as JoiValidationError } from 'joi';
 import { normalizeResponse } from '../util/helper/response.helpers';
 
 export class ErrorResponseNormalizerFilter
@@ -15,21 +14,40 @@ export class ErrorResponseNormalizerFilter
 {
   private readonly logger = new Logger(ErrorResponseNormalizerFilter.name);
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: HttpException, host: ArgumentsHost): any {
     const responseHelper = host.switchToHttp().getResponse<Response>();
-    if (exception instanceof ValidationError) {
+    if (exception instanceof JoiValidationError) {
       const response = normalizeResponse({
-        _message: exception.message,
-        _timestamp : moment().toISOString(),
         _errors: exception.details.map((detail) => ({
           key: detail.context?.key,
           message: detail.message,
           type: detail.type,
         })),
+        _message: exception.message,
       });
+
       return responseHelper
         .status(HttpStatus.UNPROCESSABLE_ENTITY)
         .json(response);
     }
+
+    if (!(exception instanceof HttpException) || !exception.getStatus) {
+      this.logger.error({
+        request: responseHelper.req.body,
+        exception: exception.stack,
+      });
+
+      return responseHelper
+        .status(exception['status'] || HttpStatus.INTERNAL_SERVER_ERROR)
+        .json(
+          normalizeResponse(
+            exception.message || 'Internal Server Error',
+          ),
+        );
+    }
+
+    return responseHelper
+      .status(exception.getStatus())
+      .json(normalizeResponse(exception.message));
   }
 }
