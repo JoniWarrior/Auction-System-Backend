11822ea159cba84a9921407c57f5fb825bc6fb7c
Edmond Alimehmeti <edi.alimehmeti@gmail.com>
2025-10-22 14:17:41 +0200

    removed entities from array

--- src/dynamic-module/db.ts
+++ src/dynamic-module/db.ts
@@ -1,9 +1,5 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Auction } from 'src/entity/auction.entity';
-import { Bidding } from 'src/entity/bidding.entity';
-import { Item } from 'src/entity/item.entity';
-import { User } from 'src/entity/user.entity';
-import { Notification } from 'src/entity/notifications.entity';
+import { join } from 'path';
 
 export const DB = TypeOrmModule.forRoot({
   type: 'postgres',
@@ -12,6 +8,7 @@ export const DB = TypeOrmModule.forRoot({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [User, Item, Bidding, Auction, Notification],
+  entities: [join(__dirname, '../**/**.entity{.ts,.js}')],
+  migrations: [join(__dirname, '../**/migrations/*{.ts,.js}')],
   synchronize: false,
 });
